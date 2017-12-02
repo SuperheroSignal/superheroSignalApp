3e63d813800f19ffdd80786b5c5ef9818a4608ac
RFeeney91 <ryansfeeney@gmail.com>
2017-12-02 11:48:11 -0800

    routes added updated server deleted schema

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-var = require("express");
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
 
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3306;
+
+// Requiring our models for syncing
 var db = require("./models");
 
-var PORT = process.env.PORT || 3000;
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var app = express();
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
-		console.log("Listening on PORT %s");
+		console.log("Listening on PORT %s", PORT);
 	});
 });
