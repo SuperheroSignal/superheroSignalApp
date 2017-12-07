790eadf6c53dd3c3c2609ca9ac3265a06371a217
leighkristine <lkroxmob@gmail.com>
2017-12-06 20:22:49 -0800

    minor css changes

---
+++ app/public/logic.js
@@ -0,0 +1,31 @@
+// logic for dynamically updating the live feed
+
+//onclick event
+$("#add-report").on("click", function(postReport) {
+	event.preventDefault();
+
+	// set variables for data
+	// parse crime type
+	var crimeTypeOrg = $("input[type=radio][name=crimeType]:checked").val();
+	var crimeType = crimeTypeOrg.split('-').join(' ');
+	console.log(crimeType);
+
+	var situation = $("#report").val().trim();
+	console.log(situation);
+
+	// create timestamp with momentjs
+	var date = moment();
+	var parseDate = "[" + date._d + "]";
+	console.log(parseDate);
+
+	// prepend data to feed
+	$(".live-feed").prepend("<div class='new-entry'>" + "<b>" + " " + crimeType + ": " + "</b>" + situation + " " + "<i>" + parseDate + "</i>" + "</div>");
+
+
+	// empty reporting form
+	$("input[type=radio][name=crimeType]:checked").attr("checked", false);
+	$("#report").val("");
+
+
+});
+
